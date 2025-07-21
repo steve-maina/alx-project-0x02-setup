faa6dcc4ebe08321eaea37bc50ee71f9ccdf1cbf
Steve Maina <stevemainam@gmail.com>
2025-07-21 15:10:47 +0300

    setup nextjs project

--- alx-project-0x02/pages/index.tsx
+++ alx-project-0x02/pages/index.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex h-screen justify-center items-center">
       <p>Welcome!</p>
     </div>
   );
